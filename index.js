15801a0e8bcac11389235d7f0fbe420881b13bab
nathaliaborges <nathaliaborges@ibm.com>
2023-03-28 21:05:16 -0300

    fix: fonts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saudacao()
     var carouselDocumentElement = document.getElementById("carousel"); 
     var liDocumentElement = carouselDocumentElement.getElementsByTagName("li"); 
     for (var i = 0; i < liDocumentElement.length - 1; i++) {
-        console.log(' teste')
+        console.log(liDocumentElement[i])
         carouselDocumentElement.appendChild(liDocumentElement[i]);
     }
 }

dae26744294f5167834382e73be08cae58fcb2b9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-11 22:37:52 -0300

    feat: form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,44 @@
-function nextImage()
-{
-    var carouselDocumentElement = document.getElementById("carousel"); 
-    var liDocumentElement = carouselDocumentElement.getElementsByTagName("li"); 
-    for (var i = 0; i < liDocumentElement.length - 1; i++) {
-        console.log(liDocumentElement[i])
-        carouselDocumentElement.appendChild(liDocumentElement[i]);
-    }
+
+function nextImage() {
+    let carouselDocumentElement = document.getElementById("carousel");
+    console.log(carouselDocumentElement.children[0]);
+    let firstChild = carouselDocumentElement.children[0];
+    lastChildIndex = carouselDocumentElement.children.length;
+    let lastChild = carouselDocumentElement.children[lastChildIndex - 1];
+    carouselDocumentElement.removeChild(lastChild);
+    carouselDocumentElement.insertBefore(lastChild, firstChild);
+
+    carouselDocumentElement.children[0].animate(  [
+        { 
+        marginLeft: '0%',
+    },
+        { 
+        marginLeft: '80%',
+        }
+      ],
+      {
+        duration: 4000     
+     })
+
+
+    carouselDocumentElement.animate(  [
+        { 
+        marginLeft: '80%',
+    },
+        { 
+        marginLeft: '0%',
+        }
+      ],
+      {
+        duration: 400     
+     })
+
+
+   
+}
+
+function lastImage() {
+    let firstChild = carouselDocumentElement.children[0];
+    carouselDocumentElement.removeChild(firstChild);
+    carouselDocumentElement.appendChild(firstChild);
 }
